321e0acd991eb8e60372ded216899fe935b08928
Slim Abida <slimabida21@gmail.com>
2026-01-24 13:19:07 +0100

    modification des classes tailwinds pour le responsive design mobile

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -3,11 +3,12 @@ import ProductCard from "./ProductCard";
 import { useAppContext } from "../context/AppContext";
 
 const BestSeller = () => {
+
   const { products } = useAppContext();
   return (
     <div >
-      <p className="text-2xl md:text-3xl font-light mt-16">Best Sellers </p>
-      <div className= "grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6">
+      <p className="text-2xl md:text-3xl font-light mt-16 ml-1">Best Sellers </p>
+      <div className= "grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 gap-4 sm:gap-3 md:gap-16 lg:grid-cols-4 lg:gap-8 justify-items-center sm:justify-items-stretch mt-6">
         {products
           .filter((product) => product.inStock)
           .slice(0, 5)
